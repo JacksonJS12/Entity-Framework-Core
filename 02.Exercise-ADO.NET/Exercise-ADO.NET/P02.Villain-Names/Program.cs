207984478a64c7dd39c0e8629f86c74c3b52b3ac
Deniz Ivanov <d.ivan0v6100@gmail.com>
2023-02-22 17:22:27 +0200

    02 is done

---
+++ 02.Exercise-ADO.NET/Exercise-ADO.NET/P02.Villain-Names/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Text;
+
+namespace P02.Villain_Names
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await using SqlConnection connection =
+                new SqlConnection(Config.ConnnectionString);
+            await connection.OpenAsync();
+
+            string result = await GetAllVilliansAndCountOfTheirMinionsAsync(sqlConnection);
+            Console.WriteLine(result);
+        }
+    }
+
+    private static async Task<string> GetAllVilliansAndCountOfTheirMinionsAsync(SqlConnection sqlConnection)
+    {
+        StringBuilder sb = new StringBuilder(); 
+
+        SqlCommand sqlCommand =
+            new SqlCommand(SqlQueries.GetAllVilliansAndCountOfTheirMinions, sqlConnection);
+        SqlDataReader reader = await sqlCommand.ExecuteReaderAsync();
+        while (reader.Read())
+        {
+            string villainName = (string)reader["Name"];
+            int minionsCount = (int)reader["MinionsCount"];
+
+
+            sb.AppendLine($"{villainName} - {minionsCount}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}   
---
+++ 02.Exercise-ADO.NET/Exercise-ADO.NET/P02.Villain-Names/SqlQueries.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02.Villain_Names
+{
+    public static class SqlQueries
+    {
+        public const string GetAllVilliansAndCountOfTheirMinions =
+            @"SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                FROM Villains AS v 
+                JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+             GROUP BY v.Id, v.Name 
+              HAVING COUNT(mv.VillainId) > 3 
+             ORDER BY COUNT(mv.VillainId)
+             ";
+    }
+}
