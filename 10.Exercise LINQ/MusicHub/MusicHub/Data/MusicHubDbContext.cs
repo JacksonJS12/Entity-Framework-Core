7b262122c474567948f81f43c527b8a1ed7d068d
Deniz Ivanov <d.ivan0v6100@gmail.com>
2023-03-23 17:17:02 +0200

    some progress

--- 10.Exercise LINQ/MusicHub/MusicHub/Data/MusicHubDbContext.cs
+++ 10.Exercise LINQ/MusicHub/MusicHub/Data/MusicHubDbContext.cs
@@ -1,30 +1,43 @@
-﻿namespace MusicHub.Data
+﻿namespace MusicHub.Data;
+
+using MusicHub.Data.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+public class MusicHubDbContext : DbContext
 {
-    using Microsoft.EntityFrameworkCore;
-
-    public class MusicHubDbContext : DbContext
+    public MusicHubDbContext()
     {
-        public MusicHubDbContext()
-        {
-        }
+    }
 
-        public MusicHubDbContext(DbContextOptions options)
-            : base(options)
-        {
-        }
+    public MusicHubDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder
-                    .UseSqlServer(Configuration.ConnectionString);
-            }
+            optionsBuilder
+                .UseSqlServer(Configuration.ConnectionString);
         }
+    }
 
-        protected override void OnModelCreating(ModelBuilder builder)
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Song>(entity =>
         {
+            entity
+                .Property(s => s.CreatedOn)
+                .HasColumnType("date");
+        });
 
-        }
+        builder.Entity<Album>(entity =>
+        {
+            entity
+                .Property(a => a.ReleaseDate)
+                .HasColumnType("date");
+        });
     }
 }
