f3919f42b0a38be87d49fa5915e330874536782d
Deniz <d.ivan0v.6100@gmail.com>
2023-03-29 01:37:38 +0300

    Lecture is done

--- 12.Exercise Advanced Querying/BookShop/BookShop.Data/BookShopContext.cs
+++ 12.Exercise Advanced Querying/BookShop/BookShop.Data/BookShopContext.cs
@@ -16,13 +16,13 @@ public class BookShopContext : DbContext
     public BookShopContext(DbContextOptions options)
     : base(options) { }
 
-    public DbSet<Book>? Books { get; set; }
+    public DbSet<Book> Books { get; set; }
 
-    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Category> Categories { get; set; }
 
-    public DbSet<Author>? Authors { get; set; }
+    public DbSet<Author> Authors { get; set; }
 
-    public DbSet<BookCategory>? BooksCategories { get; set; }
+    public DbSet<BookCategory> BooksCategories { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
