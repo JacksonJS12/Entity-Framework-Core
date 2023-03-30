498e3e845dd3cea577821fc567b2a0d3076a733a
Deniz <d.ivan0v.6100@gmail.com>
2023-03-31 01:44:07 +0300

    last commit before bed

--- 16.Exercises JSON Processing/ProductShop/ProductShop/StartUp.cs
+++ 16.Exercises JSON Processing/ProductShop/ProductShop/StartUp.cs
@@ -1,9 +1,41 @@
-﻿namespace ProductShop
+﻿using AutoMapper;
+using ProductShop.Models;
+
+namespace ProductShop;
+
+using Newtonsoft.Json;
+using ProductShop.DTOs.Import;
+
+using ProductShop.Data;
+public class StartUp
 {
-    public class StartUp
+    private static IMapper mapper;
+    public static void Main()
     {
-        public static void Main()
+        mapper = new Mapper(new MapperConfiguration(cfg =>
         {
+            cfg.AddProfile<ProductShopProfile>();
+        }));
+    }
+
+
+    public static string ImportUsers(ProductShopContext context, string inputJson)
+    {
+        var userDtos = JsonConvert.DeserializeObject<ImportUserDto[]>(inputJson);
+
+        ICollection<User> validUsers = new HashSet<User>();
+        foreach (ImportUserDto userDto in userDtos)
+        {
+            User user = mapper.Map<User>(userDto);
+
+            validUsers.Add(user);
         }
+
+        //Here we have all valid users ready for the DB
+        context.Users.AddRange(validUsers);
+        context.SaveChanges();
+
+        return $"Successfully imported {validUsers.Count}";
     }
 }
+
